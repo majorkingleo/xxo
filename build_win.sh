48c8a8727a03dce4e7c0a5bb3ccb690b086f840f
Martin Oberzalek <kingleo@gmx.at>
2022-12-01 22:31:35 +0100

    Build and crosscompiler for Windows

--- build_win.sh
+++ build_win.sh
@@ -1,4 +1,24 @@
 #!/usr/bin/env bash
 
-export PKG_CONFIG_PATH=/usr/x86_64-w64-mingw32/sys-root/mingw/lib/pkgconfig/
+MINGWDIR=/usr/x86_64-w64-mingw32/sys-root/mingw
+DLLS="Qt6Core.dll 
+Qt6Gui.dll 
+Qt6SvgWidgets.dll 
+Qt6Widgets.dll 
+libgcc_s_seh-1.dll 
+libstdc++-6.dll 
+libwinpthread-1.dll
+Qt6Svg.dll
+Qt6SvgWidgets.dll"
+
+if ! test -d dist ; then
+	mkdir dist
+	( cd dist && ln -s ../xxo.exe . )
+	( cd dist && ln -s ../*.svg . )
+	( mkdir -p dist/plugins/platforms/ && cp -a ${MINGWDIR}/plugins/platforms/*.dll dist/plugins/platforms/ )
+	for DLL in ${DLLS} ; do 
+		cp -u ${MINGWDIR}/bin/${DLL} dist
+	done
+fi
+export PKG_CONFIG_PATH=${MINGWDIR}/lib/pkgconfig/
 make -f Makefile.win
